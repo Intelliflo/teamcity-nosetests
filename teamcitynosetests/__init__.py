14ab59926c306f1dbd89dfe7dd1b374e5e70a7e1
Chris Smith <chris.smith@intelliflo.com>
2014-08-28 17:32:12 +0100

    Ad context stuff

--- teamcitynosetests/__init__.py
+++ teamcitynosetests/__init__.py
@@ -2,7 +2,6 @@ import traceback
 import operator
 from nose.plugins import Plugin
 
-
 def make_service_message(message_name, **kwargs):
     """Create service message"""
     out = ''
@@ -38,11 +37,41 @@ def escape_message(toescape):
     return out
 
 
+def name_of_context(context):
+    return context.__module__ + '.' + context.__class__.__name__
+
 class TeamCityOutput(Plugin):
     """Output test results as team city service messages"""
 
     name = 'teamcity-output'
     score = 2
 
-    def __init__(self):
-        super(TeamCityOutput, self).__init__()
+    def options(self, parser, env=os.environ):
+        super(TeamCityOutput, self).options(parser, env=env)
+
+    def configure(self, options, conf):
+        super(TeamCityOutput, self).configure(options, conf)
+        if not self.enabled:
+            return
+    
+    def finalize(self, result):
+        pass
+
+    def _report(message):
+        # todo: chuck on output stream
+
+    def startContext(self, context):
+        _report(make_service_message(
+            'testSuiteStarted', 
+            name=name_of_context(context)))
+
+    def stopContext(self, context):
+        _report(make_service_message(
+            'testSuiteFinished',
+            name=name_of_context(context)))
+    
+    def startTest(self, test):
+        pass
+
+    def stopTest(self, test):
+        pass
